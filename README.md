57f61900c715798fb35fb38ac70cdc4aa81c0e33
Rafael Leira Osuna <rafael.leira@uam.es>
2017-09-22 15:31:31 +0200

    added support to do not compile libraries

--- README.md
+++ README.md
@@ -6,15 +6,19 @@ Include the file `cmakeLimiter.cmake` in your cmake code by `include("PATH_TO_GI
 
 # Available functions
 There are the following available functions:
-- `cmakelimiter_custom_targets (onoff)`: 
+- `cmakelimiter_messages (onoff)`: 
     - onoff (boolean variable):
-        - 0:            disable the custom targets
-        - 1 (default):  enable  the custom targets
+        - 0:            disable messages
+        - 1 (default):  enable  messages
 - `cmakelimiter_compile_executables (onoff)`: 
     - onoff (boolean variable):
         - 0:            disable the executable compilation
         - 1 (default):  enable  the executable compilation
-- `cmakelimiter_messages (onoff)`: 
+- `cmakelimiter_custom_targets (onoff)`: 
     - onoff (boolean variable):
-        - 0:            disable the function messages
-        - 1 (default):  enable  the function messages
+        - 0:            disable the custom targets
+        - 1 (default):  enable  the custom targets
+- `cmakelimiter_compile_libraries (onoff)`: 
+    - onoff (boolean variable):
+        - 0:            disable the libraries compilation
+        - 1 (default):  enable  the libraries compilation
--- cmakeLimiter.cmake
+++ cmakeLimiter.cmake
@@ -4,9 +4,10 @@
 #
 # Available commands:
 #
-# - cmakelimiter_custom_targets (1/0)
-# - cmakelimiter_compile_executables (1/0)
 # - cmakelimiter_messages (1/0)
+# - cmakelimiter_compile_executables (1/0)
+# - cmakelimiter_custom_targets (1/0)
+# - cmakelimiter_compile_libraries(1/0)
 
 # limiter code
 
@@ -16,24 +17,24 @@ if (NOT CMAKE_LIMITER_NOREDIFINE)
     SET(CMAKE_LIMITER_NOREDIFINE "ON")
     cmake_minimum_required (VERSION 2.8.11)
 
-    # Limiting functions 
-    function(cmakelimiter_custom_targets onoff)
-        if (NOT NO_ADD_CUSTOM)
-            set(NO_ADD_CUSTOM "0" CACHE STRING "how many calls to cmakelimiter_compile_executables have been done" FORCE)
+    # Limiting functions
+    function(cmakelimiter_messages onoff)
+        if (NOT MESSAGE_QUIET)
+            set(MESSAGE_QUIET "0" CACHE STRING "how many calls to cmakelimiter_messages have been done" FORCE)
         endif()
         
         if (onoff MATCHES "0")
-            math(EXPR new_variable "${NO_ADD_CUSTOM}+1")
+            math(EXPR new_variable "${MESSAGE_QUIET}+1")
         else()
-            math(EXPR new_variable "${NO_ADD_CUSTOM}-1")
+            math(EXPR new_variable "${MESSAGE_QUIET}-1")
         endif()
 
         if(new_variable LESS 1)
-            unset(NO_ADD_CUSTOM CACHE)
+            unset(MESSAGE_QUIET CACHE)
         else()
-            set(NO_ADD_CUSTOM ${new_variable} CACHE STRING "how many calls to cmakelimiter_compile_executables have been done" FORCE)
+            set(MESSAGE_QUIET ${new_variable} CACHE STRING "how many calls to cmakelimiter_messages have been done" FORCE)
         endif()
-    endfunction(cmakelimiter_custom_targets)
+    endfunction(cmakelimiter_messages)
 
     function(cmakelimiter_compile_executables onoff)
         if (NOT NO_COMPILE_EXECUTABLES)
@@ -53,23 +54,41 @@ if (NOT CMAKE_LIMITER_NOREDIFINE)
         endif()
     endfunction(cmakelimiter_compile_executables)
 
-    function(cmakelimiter_messages onoff)
-        if (NOT MESSAGE_QUIET)
-            set(MESSAGE_QUIET "0" CACHE STRING "how many calls to cmakelimiter_compile_executables have been done" FORCE)
+    function(cmakelimiter_custom_targets onoff)
+        if (NOT NO_ADD_CUSTOM)
+            set(NO_ADD_CUSTOM "0" CACHE STRING "how many calls to cmakelimiter_custom_targets have been done" FORCE)
         endif()
         
         if (onoff MATCHES "0")
-            math(EXPR new_variable "${MESSAGE_QUIET}+1")
+            math(EXPR new_variable "${NO_ADD_CUSTOM}+1")
         else()
-            math(EXPR new_variable "${MESSAGE_QUIET}-1")
+            math(EXPR new_variable "${NO_ADD_CUSTOM}-1")
         endif()
 
         if(new_variable LESS 1)
-            unset(MESSAGE_QUIET CACHE)
+            unset(NO_ADD_CUSTOM CACHE)
         else()
-            set(MESSAGE_QUIET ${new_variable} CACHE STRING "how many calls to cmakelimiter_compile_executables have been done" FORCE)
+            set(NO_ADD_CUSTOM ${new_variable} CACHE STRING "how many calls to cmakelimiter_custom_targets have been done" FORCE)
         endif()
-    endfunction(cmakelimiter_messages)
+    endfunction(cmakelimiter_custom_targets)
+
+    function(cmakelimiter_compile_libraries onoff)
+        if (NOT NO_COMPILE_LIBRARIES)
+            set(NO_COMPILE_LIBRARIES "0" CACHE STRING "how many calls to cmakelimiter_compile_libraries have been done" FORCE)
+        endif()
+        
+        if (onoff MATCHES "0")
+            math(EXPR new_variable "${NO_COMPILE_LIBRARIES}+1")
+        else()
+            math(EXPR new_variable "${NO_COMPILE_LIBRARIES}-1")
+        endif()
+
+        if(new_variable LESS 1)
+            unset(NO_COMPILE_LIBRARIES CACHE)
+        else()
+            set(NO_COMPILE_LIBRARIES ${new_variable} CACHE STRING "how many calls to cmakelimiter_compile_libraries have been done" FORCE)
+        endif()
+    endfunction(cmakelimiter_compile_libraries)
 
     # Get the cmakeLimiter version
     execute_process(
@@ -106,6 +125,14 @@ if (NOT CMAKE_LIMITER_NOREDIFINE)
         if (NOT NO_COMPILE_EXECUTABLES)
             _INSTALL(${ARGN})
         else()
+        endif()
+    endfunction()
+
+    #function to remove unused libraries
+    function(add_library)
+        _add_library(${ARGN})
+        if (NO_COMPILE_LIBRARIES)
+            set_target_properties(${ARGV0} PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
         endif()
     endfunction()
 endif()
